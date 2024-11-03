8a1e0a76ec562f7c72efc8bb75cb4d494ed82633
Aqshal <aqshalbintangkrwn@gmail.com>
2024-11-03 17:56:43 +0700

    feat: added Borrowing routes

---
+++ src/routes/borrowing_routes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const BorrowingController = require("../controllers/borrowing_controller")
+
+const borrowingRouter = express.Router()
+
+borrowingRouter.get("/borrowings", BorrowingController.getAll)
+borrowingRouter.get("/borrowing/:id", BorrowingController.getById)
+borrowingRouter.post("/borrowing", BorrowingController.create)
+borrowingRouter.put("/borrowing/:id", BorrowingController.update)
+borrowingRouter.delete("/borrowing/:id", BorrowingController.delete)
+
+module.exports = borrowingRouter
